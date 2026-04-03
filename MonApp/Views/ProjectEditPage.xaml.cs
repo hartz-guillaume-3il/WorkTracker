7d708559cfc9b41bdc3705dd8727cb3b85ad316e
hartz-guillaume-3il <hartzg@3il.fr>
2026-04-03 11:51:21 +0200

    Commit Project

---
+++ MonApp/Views/ProjectEditPage.xaml.cs
@@ -0,0 +1,10 @@
+namespace WorkTracker.MonApp.Views;
+
+public partial class ProjectEditPage : ContentPage
+{
+    public ProjectEditPage()
+    {
+        InitializeComponent();
+        // TODO Partie 3.2 : affecter BindingContext = new ProjectEditViewModel();
+    }
+}
