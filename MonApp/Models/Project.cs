c77af6cec3eb005db63ccf066e1ef0c73aeb3faf
hartz-guillaume-3il <hartzg@3il.fr>
2026-04-21 17:15:39 +0200

    Update Project.cs

--- MonApp/Models/Project.cs
+++ MonApp/Models/Project.cs
@@ -1,13 +1,19 @@
+using SQLite;
+
 namespace WorkTracker.MonApp.Models
 {
+    [Table("Projects")]
     public class Project
     {
+        [PrimaryKey, AutoIncrement]
+
         public int Id { get; set; }
+
         public string Name { get; set; } = string.Empty;
         public string Description { get; set; } = string.Empty;
         public string Color { get; set; } = "#005067";
-        public string Initials => Name?.Length >= 2
-            ? Name.Substring(0, 2).ToUpper()
-            : "??";
+
+        [Ignore]
+        public string Initials => Name?.Length >= 2 ? Name.Substring(0, 2).ToUpper() : "??";
     }
 }
